5687adf26b50674d2af06b4d03a1723a99edae69
Javi <javiquiles93@gmail.com>
2022-11-18 17:59:24 +0100

    awdad

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,93 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        //Creación de del objeto escuela
+        createSchool();
+
+
+        //PASO 8
+        showMenuCommands();
+    }
+
+    //FUNCTION 1: Metodo para meter el nombre de la escuela
+    private static  SchoolManagementSystem createSchool(){
+
+        Scanner sc = new Scanner(System.in);
+        //PASO 1: dar nombre a la escuela
+        System.out.println("Enter the name of the school");
+        String schoolName = sc.nextLine();
+
+        //PASO 2: Escoger numero de profesores y poner sus detalles
+        System.out.println("Enter the number of teachers to create");
+        int numOfTeachers = sc.nextInt();
+        //PASO 3:
+        List<org.example.Teacher> teacherList = new ArrayList<>(numOfTeachers);
+        for (int i = 1; i <= numOfTeachers ; i++) {
+            System.out.println("Enter the details of teacher " + i);
+            System.out.println("Name for this teacher:");
+            String teacherName = sc.next();
+            System.out.println("Salary for this teacher:");
+            double salary = sc.nextDouble();
+            teacherList.add(new Teacher(teacherName, salary));
+        }
+        //PASO 4:
+        System.out.println("Enter the number of courses to create");
+        int numOfCourses = sc.nextInt();
+        //PASO 5:
+        List<org.example.Course> courseList = new ArrayList<>(numOfCourses);
+        for (int i = 1; i <= numOfCourses ; i++) {
+            System.out.println("Enter the details of course " + i);
+            System.out.println("Name for this course:");
+            String courseName = sc.next();
+            System.out.println("Price for this course:");
+            double price = sc.nextDouble();
+            courseList.add(new Course(courseName, price));
+        }
+        //PASO 6
+        System.out.println("Enter the number of students to create");
+        int numOfStudents = sc.nextInt();
+        //PASO 7
+        List<org.example.Student> studentList = new ArrayList<>(numOfStudents);
+        for (int i = 1; i <= numOfStudents ; i++) {
+            System.out.println("Enter the details of student " + i);
+            System.out.println("Name for this student:");
+            String studentName = sc.next();
+            System.out.println("Address for this student:");
+            String address = sc.next();
+            System.out.println("Email for this student:");
+            String email = sc.next();
+            studentList.add(new Student(studentName, address, email));
+        }
+
+       //SchoolManagementSystem school = new SchoolManagementSystem(String schoolName, List<Teacher> teacherList, List<Student> studentList, List<Course> courseList);
+        return null;
+    }
+
+    //FUNCTION 2: Metodo para escoger el numero de profesores
+
+
+
+    //FUNCTION: Método para mostrar el menú de comandos disponibles
+    private static void showMenuCommands(){
+        System.out.println("MENU \n"
+                + "1 ENROLL [STUDENT_ID] [COURSE_ID]:\n"
+                + "2 ASSIGN [TEACHER_ID] [COURSE_ID]:\n"
+                + "3 SHOW COURSES:\n"
+                + "4 LOOKUP COURSE [COURSE_ID]:\n"
+                + "5 SHOW STUDENTS:\n"
+                + "6 LOOKUP STUDENT [STUDENT_ID]:\n"
+                + "7 SHOW TEACHERS:\n"
+                + "8 LOOKUP TEACHER [TEACHER_ID]:\n"
+                + "9 SHOW PROFIT:\n"
+                + "0 EXIT");
     }
 }
--- src/main/java/org/example/SchoolManagementSystem.java
+++ src/main/java/org/example/SchoolManagementSystem.java
@@ -1,6 +1,67 @@
 package org.example;
 
+
+import java.util.List;
+import java.util.Scanner;
+
 public class SchoolManagementSystem {
+
+    private String schoolName;
+    private List<Teacher> teacherList;
+    private List<Student> studentList;
+    private List<Course> courseList;
+
+    //Constructor
+
+    public SchoolManagementSystem(String schoolName, List<Teacher> teacherList, List<Student> studentList, List<Course> courseList) {
+       setSchoolName(schoolName);
+        this.teacherList = teacherList;
+        this.studentList = studentList;
+        this.courseList = courseList;
+    }
+
+    public SchoolManagementSystem(String schoolName) {
+        setSchoolName(schoolName);
+    }
+    //Getters & Setters
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+
+        this.schoolName = schoolName;
+    }
+
+    public List<Teacher> getTeacherList() {
+        return teacherList;
+    }
+
+    public void setTeacherList(List<Teacher> teacherList) {
+        this.teacherList = teacherList;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+
+
+    //FUNCTIONS
+
     public void enroll(String studentId, String courseId){
 
     }
